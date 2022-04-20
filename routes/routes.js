05685a1085376c0fb3ab01c5da7e8dcbcaf6c410
mabiak <mabiane.polniak@gmail.com>
2022-04-20 16:35:26 -0300

    feat: service users create

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,20 @@ const express = require('express');
 const rotas = express.Router();
 
 rotas.post('user', () => {
+//  adiconar novo usuario com o seguinte formatato
+//  {
+//   "displayName": "Brett Wiltshire",
+//   "email": "brett@email.com",
+//   "password": "123456",
+//   "image": "http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png"
+// }
 
+// displayName string min 8
+// password string min 6
+// email
+//    ser unico > { message: 'User already registered' }
+//    formato blabla@blabla > 
+// Caso contrário, retornar a mesma resposta do endpoint de /login, um token JWT:
 });
 
 module.exports = rotas;
--- services/Users.js
+++ services/Users.js
@@ -1,9 +1,19 @@
 const { User } = require('../models');
 
-const getAll = async () => {
-  const allUsers = await User.findAll();
-  console.log(allUsers);
-  return allUsers;
+// const getAll = async () => {
+//   const allUsers = await User.findAll();
+//   console.log(allUsers);
+//   return allUsers;
+// };
+
+const create = async (newUser) => {
+  const user = await User.create({
+    displayName: newUser.displayName,
+    email: newUser.email,
+    password: newUser.password,
+    image: newUser.image,
+  });
+  return user;
 };
 
-module.exports = { getAll };
+module.exports = { create };
