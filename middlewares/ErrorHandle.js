52549a34b271e0e12780b89a2a8e291a259b65ab
mabiak <mabiane.polniak@gmail.com>
2022-04-22 19:30:46 -0300

    fix: tentando de novo

--- middlewares/ErrorHandle.js
+++ middlewares/ErrorHandle.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const errorHandle = (err, _req, res, _next) => {
   if (Joi.isError(err)) return res.status(400).json({ message: err.message });
-  return res.status(500).json({ message: 'Ops, algo deu errado' });
+  // return res.status(500).json({ message: 'Ops, algo deu errado' });
 };
 
 module.exports = { errorHandle };
